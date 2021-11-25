099b1a3c148a894152c355a1dfa1a18a4c95b3ec
HyunyoKim <zeus4945@naver.com>
2021-11-25 09:33:41 +0900

    컴포넌트 성능 최적화 완료

--- todo-app/src/components/TodoListItem.js
+++ todo-app/src/components/TodoListItem.js
@@ -7,19 +7,21 @@ import {
 import cn from "classnames";
 import "./TodoListItem.scss";
 
-const TodoListItem = ({ todo, onRemove, onToggle }) => {
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => {
     const { id, text, checked } = todo;
     return (
-        <div className="TodoListItem">
-            <div
-                className={cn("checkbox", { checked })}
-                onClick={() => onToggle(id)}
-            >
-                {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
-                <div className="text">{text}</div>
-            </div>
-            <div className="remove" onClick={() => onRemove(id)}>
-                <MdRemoveCircleOutline />
+        <div className="TodoListItem-virtualized" style={style}>
+            <div className="TodoListItem">
+                <div
+                    className={cn("checkbox", { checked })}
+                    onClick={() => onToggle(id)}
+                >
+                    {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+                    <div className="text">{text}</div>
+                </div>
+                <div className="remove" onClick={() => onRemove(id)}>
+                    <MdRemoveCircleOutline />
+                </div>
             </div>
         </div>
     );
@@ -28,5 +30,8 @@ const TodoListItem = ({ todo, onRemove, onToggle }) => {
 /*/
 export default TodoListItem;
 /*/ //컴포넌트의 props가 바뀌지 않았다면, 리렌더링 하지 않도록 설정
-export default React.memo(TodoListItem);
+export default React.memo(
+    TodoListItem,
+    (prevProps, nextProps) => prevProps.todo === nextProps.todo,
+);
 //*/
