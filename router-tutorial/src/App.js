0881ba35cd9648af2ffbdc69adff25163212909b
HyunyoKim <zeus4945@naver.com>
2021-11-30 17:16:47 +0900

    url 파라미터

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,14 +1,33 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
+import Profile from "./Profile";
 
 const App = () => {
   return (
     <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+        <li>
+          <Link to="/profile/velopert">velopert 프로필</Link>
+        </li>
+        <li>
+          <Link to="/profile/gildong">gildong 프로필</Link>
+        </li>
+      </ul>
+      <hr />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/info" element={<About />} />
+        <Route path="/profile/:username" element={<Profile />} />
       </Routes>
     </div>
   );
